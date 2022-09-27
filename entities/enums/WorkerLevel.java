f5a0dc1ae0afa9303e8ddc0635fb670f2c896a10
Ryan Aguiar <ryanaguiar303@gmail.com>
2022-09-27 11:32:59 -0300

    Programa para ler dados de um tabalhador com N contratos(Fornecido pelo usiario).Depois solicitar um mês e mostrar qual foi o salario do funcionario nesse mes

---
+++ entities/enums/WorkerLevel.java
@@ -0,0 +1,16 @@
+package entities.enums;
+
+public class WorkerLevel {
+
+	public enum WorkerLvel{
+		
+		JUNIOR,
+		MID_LEVEL,
+		SENIOR;
+	}
+
+	public static WorkerLevel valueOf(String workerLevel) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
